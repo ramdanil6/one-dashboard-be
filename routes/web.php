817f07109fb801d76132c6c86c88830bcf6a2450
AldoNicho <112291894+AldoNicho@users.noreply.github.com>
2024-06-27 16:09:18 +0000

    rollback web route

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,7 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    dd(env('APP_ENV', 'kosong'));
-    // return view('welcome');
+    return view('welcome');
 });
 
 Route::get('/artisan-command/{command}', function ($command) {
