6c595958e612907d048cbdfde5485e26a9616682
AldoNicho <112291894+AldoNicho@users.noreply.github.com>
2024-06-27 15:35:15 +0000

    add artisan web route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/artisan-command/{command}', function ($command) {
+    return Artisan::call($command);
 });
